52bc85b5c5d977d8f3e95a6c0d9b69090a25ef47
Adi-Vaitis <adriangabriel344@gmail.com>
2023-11-06 11:28:27 +0100

    added redux functionality for handling the login

---
+++ match3-web-app-ts/src/Pages/LoginPage/LoginPage.state.tsx
@@ -0,0 +1,71 @@
+import {User} from "../../Model/user";
+import {LoginService} from "../../Services/login.service";
+
+export interface LoginPage {
+    isFetching: boolean;
+}
+
+export const defaultLoginPage: LoginPage = {
+    isFetching: false,
+}
+
+export enum ActionTypes {
+    LOGIN_REQUEST = "LOGIN_REQUEST",
+    LOGIN_SUCCESS = "LOGIN_SUCCESS",
+    LOGIN_FAILURE = "LOGIN_FAILURE",
+}
+
+export const loginPageReducer = function (state: LoginPage = defaultLoginPage, action: any) {
+    switch (action.type) {
+        case ActionTypes.LOGIN_REQUEST:
+            return {
+                ...state,
+                isFetching: true,
+            };
+        case ActionTypes.LOGIN_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+            };
+        case ActionTypes.LOGIN_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+            };
+        default:
+            return state;
+    }
+}
+
+export const mapStateToProps = function (state: LoginPage) {
+    return {
+        isFetching: state.isFetching
+    }
+}
+
+export const fetchLogin = (dispatch: any, user: User) => {
+    const loginService = new LoginService();
+    dispatch({type: ActionTypes.LOGIN_REQUEST});
+    return loginService.login(user)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(returnedLoggedInUser => {
+            alert('User has been logged in successfully!');
+            dispatch({type: ActionTypes.LOGIN_SUCCESS, payload: returnedLoggedInUser});
+            window.location.href = '/';
+        })
+        .catch(error => {
+            alert('Error: ' + error.message);
+            dispatch({type: ActionTypes.LOGIN_FAILURE});
+        });
+}
+
+export const mapDispatchToProps = (dispatch: any) => {
+    return {
+        fetchLogin: (user: User) => fetchLogin(dispatch, user)
+    }
+}
--- match3-web-app-ts/src/Pages/LoginPage/LoginPage.tsx
+++ match3-web-app-ts/src/Pages/LoginPage/LoginPage.tsx
@@ -9,76 +9,105 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import {createTheme, ThemeProvider} from '@mui/material/styles';
+import {User} from "../../Model/user";
+import {connect, Provider} from "react-redux";
+import {mapDispatchToProps, mapStateToProps, loginPageReducer} from "./LoginPage.state";
+import {createStore} from "redux";
+import CircularProgress from "@mui/material/CircularProgress";
+import {fetchLogin} from "./LoginPage.state.js";
 
 const defaultTheme = createTheme();
 
 
-export const LoginPage = () => {
+export const LoginPageComponent = ({isFetching, fetchLogin}: any) => {
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
-        console.log({
-            username: data.get('email'),
-            password: data.get('password'),
-        });
+        const userToLogin: User = {
+            username: data.get('email') as string,
+            password: data.get('password') as string,
+        };
+        fetchLogin(userToLogin);
     };
 
     return (
         <ThemeProvider theme={defaultTheme}>
             <Container component="main" maxWidth="xs">
                 <CssBaseline />
-                <Box
-                    sx={{
+                { isFetching ? (
+                    <Box sx={{
                         marginTop: 8,
                         display: 'flex',
                         flexDirection: 'column',
                         alignItems: 'center',
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Sign In
-                        </Button>
-                        <Grid container>
-                            <Grid item>
-                                <Link href="register" variant="body2">
-                                    {"Don't have an account? Sign Up"}
-                                </Link>
-                            </Grid>
-                        </Grid>
+                    }}>
+                        <CircularProgress />
                     </Box>
-                </Box>
+                ) : (
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign in
+                        </Typography>
+                        <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{mt: 3, mb: 2}}
+                            >
+                                Sign In
+                            </Button>
+                            <Grid container>
+                                <Grid item>
+                                    <Link href="register" variant="body2">
+                                        {"Don't have an account? Sign Up"}
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+                )}
+
             </Container>
         </ThemeProvider>
+    );
+}
+
+const ReduxLoginPage = connect(mapStateToProps, mapDispatchToProps)(LoginPageComponent);
+const store = createStore(loginPageReducer);
+export const LoginPage = () => {
+    return (
+        <Provider store={store}>
+            <ReduxLoginPage/>
+        </Provider>
     );
 }
