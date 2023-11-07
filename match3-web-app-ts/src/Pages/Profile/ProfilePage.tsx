779413775a5641021a2d48599d63ffaa1c72e49b
ioanbaicoianu <140078209+ioanbaicoianu@users.noreply.github.com>
2023-11-07 22:54:22 +0100

    added profile information

--- match3-web-app-ts/src/Pages/Profile/ProfilePage.tsx
+++ match3-web-app-ts/src/Pages/Profile/ProfilePage.tsx
@@ -4,22 +4,31 @@ import {connect, Provider} from "react-redux";
 import {profileMapDispatchToProps, profileMapStateToProps, profileReducer} from "./Profile.state";
 import {createStore} from "redux";
 import {Token} from "../../Model/token";
-
+import './ProfilePage.css';
 const ProfileComponent = ({token, isFetching, user, fetchUser}: any) => {
 
     useEffect(() => {
         fetchUser((token as Token).userId);
     }, [])
 
+    const showUserInformation = () => {
+        return (
+            <>
+                {user && (
+                    <Card title="Profile" extra={<a href="#">More</a>} style={{width: 300, height: 400}}>
+                        <p>Username: {user.username}</p>
+                        <p>Admin: {user.admin ? 'Yes' : 'No'}</p>
+                    </Card>)}
+                {!user && (
+                    <div>User could not be found, you need to login again.</div>
+                    )}
+            </>
+        )
+    }
     return (
-        <Space direction="vertical" size={30}>
-            {isFetching ? (
-                <div>Loading...</div>) :
-                <Card title="Profile" extra={<a href="#">More</a>} style={{ width: 300, height: 400 }}>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-            </Card>}
+        <Space className="profile-container" direction="vertical" size={30}>
+                {isFetching ? (
+                    <div>Loading...</div>) : showUserInformation()}
         </Space>
     )
 }
@@ -27,12 +36,12 @@ const ProfileComponent = ({token, isFetching, user, fetchUser}: any) => {
 const ReduxProfilePageContainer = connect(profileMapStateToProps, profileMapDispatchToProps)(ProfileComponent);
 const store = createStore(profileReducer);
 
-export const ProfilePage = ({token} : any) => {
+export const ProfilePage = ({token}: any) => {
 
     console.log(token);
     return (
         <Provider store={store}>
-            <ReduxProfilePageContainer token={token}  userId={token.userId}/>
+            <ReduxProfilePageContainer token={token} userId={token.userId}/>
         </Provider>
     )
 }
